85aaeec4d9d91d7a320231dc61abd3d4da7118b3
FilipRank <filiprankovic22@gmail.com>
2024-10-15 19:21:35 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from './material-module';
+import { ProductionService, Production } from './services/production.service';
+import { isAwaitKeyword } from 'typescript';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MaterialModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    ProductionService
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tempAngular';
+  foo: string = "";
+  bar: Production[] = [];
+
+  constructor(private productionService: ProductionService, private ch: ChangeDetectorRef) { }
+
+  async ngOnInit() {
+    this.bar = await this.productionService.getAllProductions();
+    this.foo = (await this.productionService.getProductionById(0)).title;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, Injectable, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch())
+  ]
 };
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) {}
+}
