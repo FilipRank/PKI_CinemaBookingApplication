386569c7cecde09b19f1f864f0dc844d7be2645f
FilipRank <filiprankovic22@gmail.com>
2024-10-17 18:52:05 +0200

    add functionality to sort by description and crew

--- src/app/side-filter/side-filter.component.ts
+++ src/app/side-filter/side-filter.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { MaterialModule } from '../material-module';
 import { FormsModule } from '@angular/forms';
-import { Production } from '../services/production.service';
+import { Production, ProductionService } from '../services/production.service';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -18,13 +18,23 @@ export class SideFilterComponent {
   readonly panelOpenState = signal(false);
   @Output() filterUpdate = new EventEmitter<Production[]>();
   title: string = "";
+  description: string = "";
+  director: string = "";
+  cast: string = "";
 
-  constructor(private http: HttpClient) {}
+  constructor(private productionService: ProductionService) {}
 
   onKeyUp() {
-    this.http.get<Production[]>(`http://localhost:3000/productions?title_like=${this.title}`)
-    .subscribe((production: Production[]) => {
-      this.filterUpdate.emit(production);
+    this.productionService.getProductionsWithParams(
+      {
+        "title_like": this.title,
+        "description_like": this.description,
+        "directors_like": this.director,
+        "cast_like": this.cast
+      }
+    )
+    .subscribe((productions: Production[]) => {
+      this.filterUpdate.emit(productions);
     });
   }
 }
