715fe6581e5bcd9fd27e76c73f4c55f612eac784
FilipRank <filiprankovic22@gmail.com>
2024-10-17 18:12:25 +0200

    add query builder service to generate query argument strings

---
+++ src/app/services/queryBuilder.service.ts
@@ -0,0 +1,30 @@
+//@ts-nocheck
+
+export class QueryBuilderService {
+  private productionQueryParams = {
+    title_like: null,
+    description_like: null,
+    genre: null,
+    durationInHours_gte: null,
+    durationInHours_lte: null,
+    cast_like: null,
+    directors_like: null,
+    publishedAt_gte: null,
+    publishedAt_lte: null,
+    "ticketPrice.amount_lte": null,
+    "ticketPrice.amount_gte": null,
+    airingTimes_gte: null,
+    airingTimes_lte: null
+  }
+
+  buildArguments(params): string {
+    const queryString = Object.keys(params)
+    .filter((key) => params[key] !== undefined && params[key] !== null && params[key] !== "")
+    .map(
+      (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+    )
+    .join("&");
+
+    return queryString ? `?${queryString}` : "";
+  }
+}
