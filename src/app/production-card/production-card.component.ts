7ee03e73cedc03781ac865a6c8dfbe9910359817
FilipRank <filiprankovic22@gmail.com>
2024-10-21 12:04:48 +0200

    last commit

--- src/app/production-card/production-card.component.ts
+++ src/app/production-card/production-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { MaterialModule } from '../material-module';
 import { Production } from '../services/production.service';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-production-card',
@@ -9,8 +11,28 @@ import { Production } from '../services/production.service';
     MaterialModule
   ],
   templateUrl: './production-card.component.html',
-  styleUrl: './production-card.component.css'
+  styleUrl: './production-card.component.css',
+  providers: [
+    UserService
+  ]
 })
 export class ProductionCardComponent {
   @Input() production: Production;
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  onAddTicketToCartClick() {
+    if (!sessionStorage.getItem('active')) {
+      this.router.navigate([])
+    }
+    this.userService.getCurrentUser().subscribe((currentUser) => {
+      let newBasketItem = {
+        production: `http://localhost:3000/productions/${this.production.id}`,
+        airsAt: this.production.airingTimes[0],
+        status: "reserved",
+        amount: 1
+      };
+      this.userService.addToCartByUserId(parseInt(sessionStorage.getItem('active') as string), newBasketItem);
+    });
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, forkJoin, Observable } from "rxjs";
 
 export interface User {
     id: string,
@@ -11,13 +11,21 @@ export interface User {
     address: string,
     password: string,
     favouriteGenres: string,
-    basket: Array<{production: string, airsAt: string, status: "reserved" | "watched" | "cancelled", amount: number}>
+    basket: Array<{production: string, airsAt: string, status: string, amount: number}>
 }
 
 @Injectable()
 export class UserService {
 
   constructor(private http: HttpClient) {}
+
+  addToCartByUserId(id: number, cartItem: { production: string, airsAt: string, status: string, amount: number }) {
+    this.getUserById(id).subscribe((currentUser: User) => {
+      currentUser.basket.push(cartItem);
+      this.http.put(`http://localhost:3000/users/${id}`, currentUser)
+    })
+
+  }
 
   hasActiveUser() {
     if (sessionStorage.getItem('active')) {
@@ -26,7 +34,7 @@ export class UserService {
     return false
   }
 
-  getUserById(id: number) {
+  getUserById(id: number): Observable<User> {
     return this.http.get<User>(`http://localhost:3000/users/${id}`);
   }
 
@@ -34,11 +42,11 @@ export class UserService {
     return this.http.post<User>("http://localhost:3000/users", user);
   }
 
-  getCurrentUser(id: number) {
+  getCurrentUser() {
     if (!sessionStorage.getItem("active")) {
       throw new Error("NO CURRENT USER.")
     }
-    return this.http.get<User>(`http://localhost:3000/users/${id}`);
+    return this.http.get<User>(`http://localhost:3000/users/${sessionStorage.getItem('active')}`);
   }
 
   logoutActiveUser() {
